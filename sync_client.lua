3af9f9e9702d15c88d10f13eaae483f77d9166c2
gbMichelle <gbmichelle.dev@gmail.com>
2018-09-02 22:13:10 +0200

    Refactored the code to make it more readable.

---
+++ sync_client.lua
@@ -0,0 +1,266 @@
+-- Biped Sync Script by Michelle
+clua_version = 2.042
+
+
+
+--- Runtime structs and vars:
+tick_count = 0
+---
+
+
+set_callback("rcon message", "OnRconMessage")
+set_callback("map load", "OnMapLoad")
+set_callback("tick", "OnTick")
+
+object_array = read_u32(0x7FB710)
+
+hide_client_objects = false
+
+function OnMapLoad()
+	synced_biped_count = 0
+	
+end
+
+function OnTick()
+	if hide_client_objects == true then
+		local object_count = read_word(object_array + 0x2E)
+		for i=0, object_count-1 do
+			local object = get_object(i)
+			if object ~= nil then
+				if read_float(object+0xE0) > 0 then -- check if health is over 0, because 0 or below health bipeds should not be deleted.
+					local continue = true
+					for player=0,16 do
+						if object == get_dynamic_player(player) then
+							continue = false
+						end
+					end
+					if(read_word(object+0x52A) == 0 and continue == true) then -- object type 0 is for bipeds
+						write_bit(object + 0x10, 0, 1)
+						--[[write_bit(object + 0x10, 2, 1)
+						write_bit(object + 0x10, 7, 1)
+						write_bit(object + 0x10, 18, 1)
+						write_bit(object + 0x10, 24, 1)--]]
+					end
+				end
+			end
+		end
+	end
+end
+
+function OnRconMessage(message)
+	if string.find(message, "bi", 1) ~= nil then
+		HandleClientBaselineUpdate(message)
+		return false
+		
+	elseif string.find(message, "bs", 1) ~= nil then
+		HandleClientActionUpdate(message)
+		return false
+	
+	elseif message == "mich_sync_update" then
+		execute_script("ai false")
+		--execute_script("object_destroy_all")
+		hide_client_objects = true
+		local object_count = read_word(object_array + 0x2E)
+		local deleted_count = 0
+		for i=0, object_count-1 do
+			local object = get_object(i-deleted_count)
+			if object ~= nil then
+				if read_word(object+0xB4) == 0 then --check if object is biped
+					if read_float(object+0xE0) > 0 then -- check if health is over 0, because 0 or below health bipeds should not be deleted.
+						local continue = true
+						for player=0,16 do
+							if object == get_dynamic_player(player) then
+								continue = false
+							end
+						end
+						if(read_word(object+0x52A) == 0 and continue == true) then
+							delete_object(i-deleted_count)
+							deleted_count = deleted_count - 1
+							write_bit(object + 0x10, 0, 1)
+							write_bit(object + 0x10, 2, 1)
+							write_bit(object + 0x10, 7, 1)
+							write_bit(object + 0x10, 18, 1)
+							write_bit(object + 0x10, 24, 1)
+						end
+					end
+				end
+			end
+		end
+		return false
+	end
+end
+
+
+function HandleClientBaselineUpdate(message)
+		local unique_id = tonumber(string.sub(message, 3, 6), 16)
+		local tag_id = tonumber(string.sub(message, 7, 10), 16)
+		
+		local x = DecodePosition(tonumber(string.sub(message, 11, 16), 16))
+		local y = DecodePosition(tonumber(string.sub(message, 17, 22), 16))
+		local z = DecodePosition(tonumber(string.sub(message, 23, 28), 16))
+		
+		local rx = DecodeVector(tonumber(string.sub(message, 29, 30), 16))
+		local ry = DecodeVector(tonumber(string.sub(message, 31, 32), 16))
+		local rz = DecodeVector(tonumber(string.sub(message, 33, 34), 16))
+
+		local lx = DecodeVector(tonumber(string.sub(message, 35, 36), 16))
+		local ly = DecodeVector(tonumber(string.sub(message, 37, 38), 16))
+		local lz = DecodeVector(tonumber(string.sub(message, 39, 40), 16))
+		
+		local vx = DecodeVelocity(tonumber(string.sub(message, 41, 44), 16))
+		local vy = DecodeVelocity(tonumber(string.sub(message, 45, 48), 16))
+		local vz = DecodeVelocity(tonumber(string.sub(message, 49, 52), 16))
+		
+		local anim_id = tonumber(string.sub(message, 53, 54), 16)
+		local anim_frame = tonumber(string.sub(message, 55, 56), 16)
+		local overlay_anim_id = tonumber(string.sub(message, 57, 58), 16)
+		local overlay_anim_frame = tonumber(string.sub(message, 59, 60), 16)
+		
+		local weapon_tag_id = tonumber(string.sub(message, 61, 64), 16)
+		
+		local health = tonumber(string.sub(message, 65, 65), 16) /8
+		local shield = tonumber(string.sub(message, 66, 66), 16) /8
+		local shooting = tonumber(string.sub(message, 67, 67), 16)
+		local unit_state = tonumber(string.sub(message, 68, 69), 16)
+		
+		local object = nil
+		local object_to_edit = nil
+		
+		local object_count = read_word(object_array + 0x2E)
+		for i=0, object_count-1 do
+			object = get_object(i)
+			if object ~= nil then
+				local continue = true
+				for player=0,16 do
+					if object == get_dynamic_player(player) then
+						continue = false
+					end
+				end
+				
+				if continue == true then
+					if unique_id == read_u16(object+0x52A) then
+						object_to_edit = object
+						
+					end
+				end
+			end
+		end
+		
+		if object_to_edit == nil then
+			local object_id = spawn_object("biped", GetTagPathById(tag_id), x, y, z)
+			object_to_edit = get_object(object_id)
+			write_u16(object_to_edit+0x52A, unique_id)
+			console_out("spawn:baseline update")
+		else
+			console_out("found:baseline update")
+			write_vec3d(object_to_edit+0x5C, x, y, z)
+		end
+		
+		write_vec3d(object_to_edit+0x74, rx, ry, rz)
+		write_vec3d(object_to_edit+0x230, lx, ly, lz)
+
+		write_vec3d(object_to_edit+0x68, vx, vy, vz)
+		
+		write_u8(object_to_edit+0xD0, anim_id)
+		write_u8(object_to_edit+0xD2, anim_frame)
+		--write_u8(object_to_edit+0x2A3, overlay_anim_id)
+		--write_u8(object_to_edit+, overlay_anim_frame)
+		
+		--- weapon stuff
+		
+		write_float(object_to_edit+0xE0, health)
+		write_float(object_to_edit+0xE4, shield)
+		write_float(object_to_edit+0x284, shooting)
+		write_u8(object_to_edit+0x2A0, unit_state)
+		write_u16(object_to_edit+0x52A, unique_id)
+		
+		write_bit(object_to_edit+0x106, 11, 1) --make the biped invincible
+		
+end
+
+
+function HandleClientActionUpdate(message)
+		local unique_id = tonumber(string.sub(message, 3, 6), 16)
+
+		local x = DecodePosition(tonumber(string.sub(message, 11-4, 16-4), 16))
+		local y = DecodePosition(tonumber(string.sub(message, 17-4, 22-4), 16))
+		local z = DecodePosition(tonumber(string.sub(message, 23-4, 28-4), 16))
+		
+		local rx = DecodeVector(tonumber(string.sub(message, 29-4, 30-4), 16))
+		
+		local lx = DecodeVector(tonumber(string.sub(message, 35-8, 36-8), 16))
+		local ly = DecodeVector(tonumber(string.sub(message, 37-8, 38-8), 16))
+
+		
+		local anim_id = tonumber(string.sub(message, 31, 32), 16)
+		local overlay_anim_id = tonumber(string.sub(message, 33, 34), 16)
+		
+		local vx = DecodeVelocity(tonumber(string.sub(message, 35, 38), 16))
+		local vy = DecodeVelocity(tonumber(string.sub(message, 39, 42), 16))
+		local vz = DecodeVelocity(tonumber(string.sub(message, 43, 46), 16))
+		
+		local object = nil
+		local object_to_edit = nil
+		
+		local object_count = read_word(object_array + 0x2E)
+		for i=0, object_count-1 do
+			object = get_object(i)
+			if object ~= nil then
+				local continue = true
+				for player=0,16 do
+					if object == get_dynamic_player(player) then
+						continue = false
+					end
+				end
+				
+				if continue == true then
+					if unique_id == read_u16(object+0x52A) then
+						object_to_edit = object
+						console_out("found:action update")
+					end
+				end
+			end
+		end
+		
+		if object_to_edit == nil then
+			return nil
+		end
+		
+		write_vec3d(object_to_edit+0x5C, x, y, z)
+		
+		write_float(object_to_edit+0x74, rx)
+		write_float(object_to_edit+0x230, lx)
+		write_float(object_to_edit+0x230+4, lx)
+		
+		write_u8(object_to_edit+0xD0, anim_id)
+		--write_u8(object_to_edit+0x2A3, overlay_anim_id)
+		
+		--write_vec3d(object_to_edit+0x68, vx, vy, vz)
+		
+		write_u16(object_to_edit+0x52A, unique_id)
+end
+
+
+function DecodePosition(input)
+	return input/(2^24)*10000 -5000
+end
+
+function DecodeVector(input)
+	return (input/127 -1)
+end
+
+function DecodeVelocity(input)
+	return input/((2^16-1)/5) -2.5
+end
+
+function write_vec3d(dest, i, j, k)
+	write_f32(dest, i)
+	write_f32(dest+4, j)
+	write_f32(dest+8, k)
+end
+
+
+function GetTagPathById(id)
+	return read_string8(read_u32(0x40440028 + 0x20*id + 0x10))
+end
+
---
+++ sync_server.lua
@@ -0,0 +1,383 @@
+-- Biped Sync Script by Michelle
+api_version = "1.9.0.0"
+
+
+--- Settings:
+ticks_per_update = 8 --determines the update rate
+
+
+
+---
+
+
+--- Runtime structs and vars:
+tick_count = 0
+synced_biped_count = 0
+last_added_biped_id = 0
+synced_bipeds = {}
+
+--- synced biped struct
+synced_bipeds_found = {}
+synced_bipeds_unique_id = {}
+synced_bipeds_object_id = {}
+synced_bipeds_tag_id = {}
+
+synced_bipeds_pos = {}
+synced_bipeds_pos_x = {}
+synced_bipeds_pos_y = {}
+synced_bipeds_pos_z = {}
+synced_bipeds_rot_x = {}
+synced_bipeds_rot_y = {}
+synced_bipeds_rot_z = {}
+synced_bipeds_look_x = {}
+synced_bipeds_look_y = {}
+synced_bipeds_look_z = {}
+synced_bipeds_vel_x = {}
+synced_bipeds_vel_y = {}
+synced_bipeds_vel_z = {}
+
+synced_bipeds_animation_id = {}
+synced_bipeds_animation_frame = {}
+synced_bipeds_overlay_animation_id = {}
+synced_bipeds_overlay_animation_frame = {}
+
+synced_bipeds_weapon_tag_id = {}
+synced_bipeds_team = {}
+synced_bipeds_red = {}
+synced_bipeds_green = {}
+synced_bipeds_blue = {}
+synced_bipeds_health = {}
+synced_bipeds_shield = {}
+synced_bipeds_shooting = {}
+synced_bipeds_unit_state = {}
+synced_bipeds_perm1 = {}
+synced_bipeds_perm2 = {}
+synced_bipeds_perm3 = {}
+synced_bipeds_perm4 = {}
+synced_bipeds_perm5 = {}
+synced_bipeds_perm6 = {}
+synced_bipeds_perm7 = {}
+synced_bipeds_perm8 = {}
+synced_bipeds_needs_update = {}
+synced_bipeds_tag_id_stuff = {}
+---
+syncing_enabled = true
+object_array = nil
+
+function OnError(Message)
+    print(debug.traceback())
+end
+
+
+function OnScriptLoad()
+	register_callback(cb['EVENT_GAME_END'], "OnGameEnd")
+	register_callback(cb['EVENT_GAME_START'], "OnGameStart")
+	register_callback(cb['EVENT_TICK'], "OnTick")
+	syncing_enabled = true
+	object_array = get_object_array()
+	cprint("test1")
+end
+
+
+function OnGameEnd()
+	synced_biped_count = 0
+	last_added_biped_id = 0
+	syncing_enabled = false
+end
+
+
+function OnGameStart()
+	synced_biped_count = 0
+	last_added_biped_id = 0
+	syncing_enabled = true
+	tick_count = 0
+end
+
+
+function OnTick()
+	tick_count = tick_count + 1 -- fuck off lua, why the fuck would you not support += or ++
+		--- Set found values from last update to false
+	for i=1,synced_biped_count do
+		if synced_bipeds_found[i] == true then
+			synced_bipeds_found[i] = false
+			synced_bipeds_needs_update[i] = false
+		end
+	end
+	
+	--- Iterate through the object_array
+	for i=0, get_object_count()-1 do
+		local object = get_object(i)
+		if(object ~= nil) then
+			if(read_word(object+0xB4) == 0) then -- object type 0 is for bipeds
+				UpdateBipedInfo(object, i)
+			end
+		end -- turn into else if chain for other object types, because we can't have switch cases
+	end
+		
+	if (math.mod(tick_count,ticks_per_update) == 0) then
+		for player=1,16 do
+			rprint(player, "mich_sync_update")
+		end
+	end
+	
+	SendBipedInfo()
+	CleanUpBipedInfo()
+	
+end
+
+
+function UpdateBipedInfo(m_object, m_object_id)
+	--- Get all relevant biped info
+	
+	for p=1,16 do
+		if get_dynamic_player(p) == m_object then
+			--cprint("Ignored player biped")
+			return 0
+		end
+	end
+	
+	if (read_float(m_object+0xE0) > 0) then --Check if health is valid
+		local synced_biped_to_edit = nil
+		
+		local unique_id = read_word(m_object+0x52A)
+		if (unique_id ~= 0) then
+			for i=1, synced_biped_count do
+				if synced_bipeds_unique_id[i] == unique_id then
+					synced_bipeds_found[i] = true
+					synced_biped_to_edit = i
+				end
+			end
+		else
+			synced_biped_count = synced_biped_count+1
+			last_added_biped_id = last_added_biped_id + 1
+			unique_id = last_added_biped_id
+			write_word(m_object+0x52A, unique_id)
+			
+			for i=1, synced_biped_count do
+				if synced_bipeds_unique_id[i] == nil then
+					synced_bipeds_found[i] = true
+					synced_bipeds_unique_id[i] = unique_id
+					synced_biped_to_edit = i
+				end
+			end
+		end
+		
+		if synced_biped_to_edit ~= nil then
+			synced_bipeds_object_id[synced_biped_to_edit] = m_object_id
+			synced_bipeds_tag_id[synced_biped_to_edit] = read_word(m_object)
+			synced_bipeds_tag_id_stuff[synced_biped_to_edit] = read_word(m_object+2)
+
+			synced_bipeds_pos_x[synced_biped_to_edit] = read_float(m_object + 0x5C)
+			synced_bipeds_pos_y[synced_biped_to_edit] = read_float(m_object + 0x5C+4)
+			synced_bipeds_pos_z[synced_biped_to_edit] = read_float(m_object + 0x5C+8)
+			if synced_bipeds_rot_x[synced_biped_to_edit] ~= nil then
+				local test = synced_bipeds_rot_x[synced_biped_to_edit] - read_float(m_object + 0x74)
+				if test <0 then
+					test = test*-1
+				end
+				if test > 0.20 then
+					synced_bipeds_needs_update[synced_biped_to_edit] = true
+				end
+			end
+			synced_bipeds_rot_x[synced_biped_to_edit] = read_float(m_object + 0x74)
+			synced_bipeds_rot_y[synced_biped_to_edit] = read_float(m_object + 0x74+4)
+			synced_bipeds_rot_z[synced_biped_to_edit] = read_float(m_object + 0x74+8)
+			synced_bipeds_look_x[synced_biped_to_edit] = read_float(m_object + 0x224)
+			synced_bipeds_look_y[synced_biped_to_edit] = read_float(m_object + 0x224+4)
+			synced_bipeds_look_z[synced_biped_to_edit] = read_float(m_object + 0x224+8)
+			synced_bipeds_vel_x[synced_biped_to_edit] = read_float(m_object + 0x68)
+			synced_bipeds_vel_y[synced_biped_to_edit] = read_float(m_object + 0x68+4)
+			synced_bipeds_vel_z[synced_biped_to_edit] = read_float(m_object + 0x68+8)
+			
+			if synced_bipeds_animation_id[synced_biped_to_edit] ~= read_word(m_object + 0xD0) then
+				synced_bipeds_needs_update[synced_biped_to_edit] = true
+			end
+			synced_bipeds_animation_id[synced_biped_to_edit] = read_word(m_object + 0xD0)
+			synced_bipeds_animation_frame[synced_biped_to_edit] = read_word(m_object + 0xD2) --might not be right
+			synced_bipeds_overlay_animation_id[synced_biped_to_edit] = read_word(m_object + 0x2A3) --- actually state
+			synced_bipeds_overlay_animation_frame[synced_biped_to_edit] = 0
+
+			synced_bipeds_weapon_tag_id[synced_biped_to_edit] = 0
+			synced_bipeds_team[synced_biped_to_edit] = 0
+
+			synced_bipeds_health[synced_biped_to_edit] = read_float(m_object + 0xE0)
+			synced_bipeds_shield[synced_biped_to_edit] = read_float(m_object + 0xE4)
+			synced_bipeds_shooting[synced_biped_to_edit] = read_float(m_object + 0x284)
+			synced_bipeds_unit_state[synced_biped_to_edit] = read_byte(m_object + 0x2A0)
+			
+			synced_bipeds_red[synced_biped_to_edit] = read_float(m_object + 0x188)
+			synced_bipeds_green[synced_biped_to_edit] = read_float(m_object + 0x188+4)
+			synced_bipeds_blue[synced_biped_to_edit] = read_float(m_object + 0x188+8)
+			synced_bipeds_perm1[synced_biped_to_edit] = read_byte(m_object + 0x180)
+			synced_bipeds_perm2[synced_biped_to_edit] = read_byte(m_object + 0x180+1)
+			synced_bipeds_perm3[synced_biped_to_edit] = read_byte(m_object + 0x180+2)
+			synced_bipeds_perm4[synced_biped_to_edit] = read_byte(m_object + 0x180+3)
+			synced_bipeds_perm5[synced_biped_to_edit] = read_byte(m_object + 0x180+4)
+			synced_bipeds_perm6[synced_biped_to_edit] = read_byte(m_object + 0x180+5)
+			synced_bipeds_perm7[synced_biped_to_edit] = read_byte(m_object + 0x180+6)
+			synced_bipeds_perm8[synced_biped_to_edit] = read_byte(m_object + 0x180+7)
+		end
+	end
+end
+
+
+function SendBipedInfo()
+	cprint("Synced biped count:" .. synced_biped_count)
+	for sb=1,synced_biped_count-1 do
+		if synced_bipeds_found[sb] == true then
+			if (math.mod(tick_count,ticks_per_update) == 0) then
+				synced_bipeds_needs_update[sb] = false
+				local string_to_send = "bi"
+				local debug_string = "bi"
+				string_to_send = string_to_send .. string.format("%04X", synced_bipeds_unique_id[sb])
+				string_to_send = string_to_send .. string.format("%04X", synced_bipeds_tag_id[sb])
+				
+				string_to_send = string_to_send .. string.format("%06X", CompressPosition(synced_bipeds_pos_x[sb]))
+				string_to_send = string_to_send .. string.format("%06X", CompressPosition(synced_bipeds_pos_y[sb]))
+				string_to_send = string_to_send .. string.format("%06X", CompressPosition(synced_bipeds_pos_z[sb]))
+			
+				string_to_send = string_to_send .. string.format("%02X", CompressVector(synced_bipeds_rot_x[sb]))
+				string_to_send = string_to_send .. string.format("%02X", CompressVector(synced_bipeds_rot_y[sb]))
+				string_to_send = string_to_send .. string.format("%02X", CompressVector(synced_bipeds_rot_z[sb]))
+			
+				string_to_send = string_to_send .. string.format("%02X", CompressVector(synced_bipeds_look_x[sb]))
+				string_to_send = string_to_send .. string.format("%02X", CompressVector(synced_bipeds_look_y[sb]))
+				string_to_send = string_to_send .. string.format("%02X", CompressVector(synced_bipeds_look_z[sb]))
+			
+				string_to_send = string_to_send .. string.format("%04X", CompressVelocity(synced_bipeds_vel_x[sb]))
+				string_to_send = string_to_send .. string.format("%04X", CompressVelocity(synced_bipeds_vel_y[sb]))
+				string_to_send = string_to_send .. string.format("%04X", CompressVelocity(synced_bipeds_vel_z[sb]))
+			
+				string_to_send = string_to_send .. string.format("%02X", synced_bipeds_animation_id[sb])
+				string_to_send = string_to_send .. string.format("%02X", synced_bipeds_animation_frame[sb])
+				string_to_send = string_to_send .. string.format("%02X", synced_bipeds_overlay_animation_id[sb])
+				string_to_send = string_to_send .. string.format("%02X", synced_bipeds_overlay_animation_frame[sb])
+			
+				string_to_send = string_to_send .. string.format("%04X", synced_bipeds_weapon_tag_id[sb])
+
+				if synced_bipeds_health[sb] < 0 then
+					string_to_send = string_to_send .. "0"
+				elseif synced_bipeds_health[sb] > 1 then
+					string_to_send = string_to_send .. "1"
+				else
+					string_to_send = string_to_send .. string.format("%01X", math.floor((synced_bipeds_health[sb])*8))
+				end
+			
+				if synced_bipeds_shield[sb] < 0 then
+					string_to_send = string_to_send .. "0"
+				elseif synced_bipeds_shield[sb] > 1 then
+					string_to_send = string_to_send .. "1"
+				else
+					string_to_send = string_to_send .. string.format("%01X", math.floor((synced_bipeds_shield[sb])*8))
+				end
+			
+				if synced_bipeds_shooting[sb] ~= 0 then
+					string_to_send = string_to_send .. "1"
+				else
+					string_to_send = string_to_send .. "0"
+				end
+				
+				string_to_send = string_to_send .. string.format("%02X", synced_bipeds_unit_state[sb])
+				for player=1,16 do
+					rprint(player, string_to_send)
+				end
+				
+			elseif synced_bipeds_needs_update[sb] == true then
+				synced_bipeds_needs_update[sb] = false
+				local string_to_send = "bs"
+				
+				string_to_send = string_to_send .. string.format("%04X", synced_bipeds_unique_id[sb])
+				
+				string_to_send = string_to_send .. string.format("%06X", CompressPosition(synced_bipeds_pos_x[sb]))
+				string_to_send = string_to_send .. string.format("%06X", CompressPosition(synced_bipeds_pos_y[sb]))
+				string_to_send = string_to_send .. string.format("%06X", CompressPosition(synced_bipeds_pos_z[sb]))
+				
+				string_to_send = string_to_send .. string.format("%02X", CompressVector(synced_bipeds_rot_x[sb]))
+				
+				string_to_send = string_to_send .. string.format("%02X", CompressVector(synced_bipeds_look_x[sb]))
+				string_to_send = string_to_send .. string.format("%02X", CompressVector(synced_bipeds_look_y[sb]))
+				
+				string_to_send = string_to_send .. string.format("%02X", synced_bipeds_animation_id[sb])
+				string_to_send = string_to_send .. string.format("%02X", synced_bipeds_overlay_animation_id[sb])
+				
+				string_to_send = string_to_send .. string.format("%04X", CompressVelocity(synced_bipeds_vel_x[sb]))
+				string_to_send = string_to_send .. string.format("%04X", CompressVelocity(synced_bipeds_vel_y[sb]))
+				string_to_send = string_to_send .. string.format("%04X", CompressVelocity(synced_bipeds_vel_z[sb]))
+				
+				
+				for player=1,16 do
+					rprint(player, string_to_send)
+				end
+				
+			end
+			
+		end
+	end
+	
+end
+
+
+function CleanUpBipedInfo()
+	local deleted = 0
+	for i=1,synced_biped_count do
+		if synced_bipeds_found[i] == false then
+			synced_bipeds_found[i] = nil
+			synced_bipeds_unique_id[i] = nil
+		end
+	end
+end
+
+
+function CompressPosition(input)
+	local output = math.floor((input+5000)*((2^24)/10000))
+	if output < 0 then
+		output = 0
+	elseif output > 2^24-1 then
+		output = 2^24-1
+	end
+	return output
+end
+
+function CompressVector(input)
+	return (math.floor((input+1)*127))
+end
+
+function CompressVelocity(input)
+	if input > 2.5 then
+		return 2^16-1
+	elseif input < -2.5 then
+		return 0
+	end
+	return math.floor((input+2.5)*((2^16-1)/5))
+end
+
+
+
+
+
+
+
+
+--- This function is made to mimic the chimera api version
+function get_object(object_id)
+	local object_pointer = nil
+	
+	object_pointer = read_dword(object_array + 0x38 + object_id * 0xC + 0x8)
+	if object_pointer == 0 or object_pointer == 0xFFFFFFFF then
+		object_pointer = nil
+	end
+
+	return object_pointer
+end
+
+function get_object_count()
+	return read_word(object_array + 0x2E)
+end
+
+function get_object_array()
+	return read_dword(read_dword(sig_scan("8B0D????????8B513425FFFF00008D") + 2))
+end
+
+function read_vec3d(address)
+	return read_vector3d(address)
+end
